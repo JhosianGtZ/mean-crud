36742c1ae7435b1e916981c0d4fe12038c2fb40d
Jhos <jhosianlpz@gmail.com>
2022-07-12 16:20:16 -0700

    Models

---
+++ backend/models/Employee.js
@@ -0,0 +1,25 @@
+const moongoose = require('mongoose')
+const Schema = moongoose.Schema;
+
+//Define collection and Schema
+
+let employee = new Schema({
+
+
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  destination: {
+    type: String
+  },
+  phoneNumber: {
+    type: Number
+  }
+}, {
+  collection: 'employees'
+})
+
+module.exports = moongoose.model('Employee', Employee)
